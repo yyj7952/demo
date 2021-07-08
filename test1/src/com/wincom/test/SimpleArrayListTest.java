c1cf912067ea92b7a80f52325d01254b3cb0901e
yyj7952 <yyj7952@gmail.com>
2021-07-08 14:45:05 +0900

    sa22

--- test1/src/com/wincom/test/SimpleArrayListTest.java
+++ test1/src/com/wincom/test/SimpleArrayListTest.java
@@ -9,7 +9,7 @@ public class SimpleArrayListTest {
         int intValue1 = intList.get(0); // 형변환이 필요없다
         int intValue2 = intList.get(1); // 형변환이 필요없다
 
-        //String strValue = intList.get(0); // 컴파일에러 dssdfs
+        //String strValue = intList.get(0); // 컴파일에러 dssdfsads
 
         /*SimpleArrayList list = new SimpleArrayList();
 
